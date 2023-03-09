514fa3c2533f06a8bdc0ef0e412dcf07ceb86735
admin <>
2023-03-09 13:45:07 +0900

    BackGround & Camerashake

--- Step_01/Assets/PlayerController.cs
+++ Step_01/Assets/PlayerController.cs
@@ -23,6 +23,11 @@ public class PlayerController : MonoBehaviour
     // ** 복제할 총알 원본
     public GameObject BulletPrefab;
 
+    // ** 복제할 FX 원본
+    public GameObject fxPrefab;
+
+    public GameObject[] stageBack = new GameObject[7];
+
     // ** 복제된 총알의 저장공간.
     private List<GameObject> Bullets = new List<GameObject>();
 
@@ -51,6 +56,9 @@ public class PlayerController : MonoBehaviour
         onAttack = false;        
         onHit = false;
         Direction = 1.0f;
+
+        for (int i = 0; i < 7; ++i)
+            stageBack[i] = GameObject.Find(i.ToString());
     }
 
     // ** 유니티 기본 제공 함수
@@ -101,6 +109,9 @@ public class PlayerController : MonoBehaviour
             // ** 총알 스크립트내부의 방향 변수를 현재 플레이어의 방향 변수로 설정 한다.
             Controller.Direction = new Vector3(Direction, 0.0f, 0.0f);
 
+            // ** 총알 스크립트내부의 FX Prefab을 설정한다.
+            Controller.fxPrefab = fxPrefab;
+
             // ** 총알의 SpriteRenderer를 받아온다.
             SpriteRenderer buleltRenderer = Obj.GetComponent<SpriteRenderer>();
 
@@ -115,7 +126,9 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("Speed", Hor);
 
         // ** 실제 플레이어를 움직인다.
-        transform.position += Movement;
+
+        // ** offset box 
+        //transform.position += Movement;
     }
 
     private void OnAttack()
