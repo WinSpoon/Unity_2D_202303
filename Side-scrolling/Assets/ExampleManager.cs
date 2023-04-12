acd4e7597caed7cc01d4b3f489b408580cd7b448
admin <>
2023-04-12 13:45:13 +0900

    Security

--- Side-scrolling/Assets/ExampleManager.cs
+++ Side-scrolling/Assets/ExampleManager.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text;
 using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
+using System.Security.Cryptography;
 
 
 [System.Serializable]
@@ -29,8 +31,25 @@ public class ExampleManager : MonoBehaviour
 {
     string URL = "https://script.google.com/macros/s/AKfycbzUO8EkTbTm-vESBioS89cJVdACLDY1xaK197eBOtF7O8MiPMHSR0VLf1HCjhiYOVyL/exec";
 
+
+    public string SHA256Hash(string data)
+    {
+        SHA256 sha = new SHA256Managed();
+        byte[] hash = sha.ComputeHash(Encoding.ASCII.GetBytes(data));
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (byte b in hash)
+        {
+            stringBuilder.AppendFormat("{0:x2}", b);
+        }
+        return stringBuilder.ToString();
+    }
+
+
     IEnumerator Start()
     {
+        print(SHA256Hash("zzzz"));
+
+
         /*
         // ** 요청을 하기위한 작업.
         MemberForm member = new MemberForm("변사또", 45);
@@ -48,6 +67,7 @@ public class ExampleManager : MonoBehaviour
             print(request.downloadHandler.text);
         }
          */
+
 
         using (UnityWebRequest request = UnityWebRequest.Get(URL))
         {
@@ -67,4 +87,15 @@ public class ExampleManager : MonoBehaviour
     {
         SceneManager.LoadScene("progressScene");
     }
+
+
+    /*
+    void function()
+    {
+        // ** 게스트 모드 식별자
+        //System.Guid uid = System.Guid.NewGuid();
+        //return uid.ToString();
+    }
+     */
+
 }
