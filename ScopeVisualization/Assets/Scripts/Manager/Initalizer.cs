67b97b8a9ad1a035c8a00f29d32a3dae19815ae5
admin <>
2023-04-27 13:47:45 +0900

    Update

---
+++ ScopeVisualization/Assets/Scripts/Manager/Initalizer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initalizer : MonoBehaviour
+{
+    private void Awake()
+    {
+        var prefabManager = PrefabManager.GetInstance;
+    }
+}
